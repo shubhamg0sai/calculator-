fc26b6f455c5cee1c1d49a19e7269fbae1c61517
ʕ´•ᴥ•`ʔ╠ŞĦỮβĦa̷m̷╣ʕ´•ᴥ•`ʔ <63947251+shubhamg0sai@users.noreply.github.com>
2022-09-21 17:11:13 +0530

    Update MainActivity.java

--- android/app/Java/com.example/MainActivity.java
+++ android/app/Java/com.example/MainActivity.java
@@ -60,33 +60,23 @@ public class MainActivity extends AppCompatActivity {
 
 
 public class MainActivity extends AppCompatActivity {
-
-Button b1;
-
-EditText 11, 12;
-
-TextView ti;
-
-@Override
-
-protected void onCreate(Bundle savedInstanceState) {
-
-super.onCreate(savedInstanceState);
-
-setContentView(R.layout.activity_main);
-
-n1 = findViewById(R.id.number1);
-n2 = findViewById(R.id.number2);
-b1 = findViewById(R.id.button1);
-t1 = findViewById(R.id.text1);
-
-bl.setOnClickListener(new View.OnClickListener() {
-
-@Override
+    Button b1;
+    EditText 11, 12;
+    TextView ti;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        n1 = findViewById(R.id.number1);
+        n2 = findViewById(R.id.number2);
+        b1 = findViewById(R.id.button1);
+        t1 = findViewById(R.id.text1);
+        bl.setOnClickListener(new View.OnClickListener() {
+            @Override
 
 public void onClick(View view) {
 
-(t1.getText().toString().length()==0);
+if (t1.getText().toString().length()==0);
 
 int p = Integer.parseInt(n1.getText().toString());
 
@@ -98,7 +88,7 @@ ti.setText(String.valueOf(sum));
 
 }
 }
-}
+);
 
 
 
