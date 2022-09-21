b45f263fb534cbb3a4e72c836f634f5cc4626782
ʕ´•ᴥ•`ʔ╠ŞĦỮβĦa̷m̷╣ʕ´•ᴥ•`ʔ <63947251+shubhamg0sai@users.noreply.github.com>
2022-09-21 16:57:31 +0530

    Update MainActivity.java

--- android/app/Java/com.example/MainActivity.java
+++ android/app/Java/com.example/MainActivity.java
@@ -45,3 +45,56 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+Button b1;
+
+EditText 11, 12;
+
+TextView ti;
+
+@Override
+
+protected void onCreate(Bundle savedInstanceState) {
+
+super.onCreate(savedInstanceState);
+
+setContentView(R.layout.activity_main);
+
+n1 = findViewById(R.id.number1);
+n2 = findViewById(R.id.number2);
+b1 = findViewById(R.id.button1);
+
+findViewById(R.id.text1); t1 =
+
+bl.setOnClickListener(new View.OnClickListener() {
+
+@Override
+
+public void onClick(View view) {
+
+(t1.getText().toString().length()==0);
+
+I
+
+int p = Integer.parseInt(n1.getText().toString());
+
+int m = Integer.parseInt(n2.getText().toString());
+
+int sum = p + m;
+
+ti.setText(String.valueOf(sum));
